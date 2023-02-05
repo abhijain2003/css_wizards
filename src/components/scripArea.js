360102bced0671c0083a0d013884872714cde03f
Senthil Kumar P <senthilm784@gmail.com>
2023-02-05 15:52:23 +0530

    added drag and drop feature

--- src/components/scripArea.js
+++ src/components/scripArea.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/scriptArea.css";
+import { Rnd } from "react-rnd";
 
 const ScripArea = ({
   width,
@@ -9,18 +10,29 @@ const ScripArea = ({
   backgroundColor,
   borderWidth,
 }) => {
+  const [position, setPosition] = useState({
+    x: 309,
+    y: 39,
+  });
   return (
-    <div className="main_parent_play_box">
-      <div
-        style={{
-          width: `${width}px`,
-          height: `${height}px`,
-          border: `${borderWidth}px solid ${borderColor}`,
-          borderRadius: `${borderRadius}px`,
-          backgroundColor: backgroundColor,
-        }}
-      ></div>
-    </div>
+    <Rnd
+      position={{ x: position.x, y: position.y }}
+      onDragStop={(e, d) => {
+        setPosition({ ...position, x: d.x, y: d.y });
+      }}
+    >
+      <div className="main_parent_play_box">
+        <div
+          style={{
+            width: `${width}px`,
+            height: `${height}px`,
+            border: `${borderWidth}px solid ${borderColor}`,
+            borderRadius: `${borderRadius}px`,
+            backgroundColor: backgroundColor,
+          }}
+        ></div>
+      </div>
+    </Rnd>
   );
 };
 
